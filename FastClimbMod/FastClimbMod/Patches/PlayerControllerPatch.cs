2cea8f77058671fd3118cef15202a3179a7a3adf
Lionel Verano <lionel.verano.iv@berkeley.edu>
2023-11-25 19:47:43 -0800

    made first mod

---
+++ FastClimbMod/FastClimbMod/Patches/PlayerControllerPatch.cs
@@ -0,0 +1,21 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastClimbMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        private static void Postfix(ref float ___climbSpeed)
+        {
+            ___climbSpeed = 20f;
+        }
+    }
+}
---
+++ FastClimbMod/FastClimbMod/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastClimbMod
+{
+    [BepInPlugin(modGUID, modBase, modVer)]
+    public class ClimbMod : BaseUnityPlugin
+    {
+        private const string modGUID = "lionelvlv.LCFastClimbMod";
+        private const string modBase = "FastClimbMod";
+        private const string modVer = "1.0.0";
+
+        private readonly Harmony harmony = new HarmonyLib.Harmony(modGUID);
+        private static ClimbMod Instance;
+        internal ManualLogSource wls;
+        void Awake()
+        {
+            if (Instance == null) { 
+                Instance = this;
+            }
+            wls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            wls.LogInfo("FastClimbMod loaded");
+            harmony.PatchAll(typeof(ClimbMod));
+            harmony.PatchAll(typeof(Patches.PlayerControllerPatch));
+        }
+    }
+}
